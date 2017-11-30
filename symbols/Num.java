8321155efd525c02a245f801cabbefd4b1a8d261
Kev <ngynkvn@gmail.com>
2017-11-29 20:34:10 -0500

    Renamed number to num

---
+++ symbols/Num.java
@@ -0,0 +1,9 @@
+package symbols;
+
+public interface Num
+{
+    // Num plus(Num other);
+    // Num minus(Num other);
+    // Num multiply(Num other);
+    // Num divide(Num other);
+}
--- symbols/Number.java
+++
@@ -1,9 +0,0 @@
-package symbols;
-
-public interface Number
-{
-    Number plus(Number other);
-    Number minus(Number other);
-    Number multiply(Number other);
-    Number divide(Number other);
-}
