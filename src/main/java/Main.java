4b709b18bdffc7eb95a6e057e116840da0adc923
Kev <ngynkvn@gmail.com>
2017-12-06 21:31:45 -0500

    Added more main functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,60 +1,76 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import symbols.*;
 
-public class Main
-{
+public class Main {
     private static final Scanner in = new Scanner(System.in);
     private static final Calculator calc = new Calculator(null);
-    public static void main(String [] args)
-    {
-        while(true)
-        {
+    private static ArrayList<String> prev = new ArrayList<>();
+
+    public static void main(String[] args) {
+        while (true) {
             printMenu();
             int option = getInput();
-            switch(option)
-            {
-                case 1:
-                Rational ans = calc.parseExpression(getExpression());
-                System.out.printf("Your answer is : %s\n",ans);
+            System.out.println();
+            switch (option) {
+            case 1:
+                try {
+                    String exp = getExpression();
+                    System.out.println();
+                    Rational ans = calc.parseExpression(exp);
+                    System.out.printf("Your answer is: %s\n\n",ans);
+                    prev.add(String.format("%s = %s", exp, ans));
+                    if(prev.size() > 10)
+                        prev.remove(0);
+                } catch (Exception e) {
+                    System.out.println("Unexpected input. Please input a valid expression.");
+                }
                 break;
-                case 2:
+            case 2:
                 printHelp();
                 break;
-                case 3:
-                
+            case 3:
+                printPrev();
                 break;
-                case 4:
+            case 4:
                 in.close();
                 return;
             }
         }
     }
-    private static void printMenu()
-    {
-        System.out.println("1. Compute new expression\n2. Help\n3. Review previous expressions and answers\n4. Quit");
-    }
-    private static void printHelp()
-    {
 
+    private static void printMenu() {
+        System.out.printf("1. %s\n2. %s\n3. %s\n4. %s\n\nInput: ", "Compute new expression", "Help",
+                "Review previous expressions and answers", "Quit");
     }
-    private static String getExpression()
-    {
+
+    private static void printHelp() {
+        System.out.printf("Just figure it out.\n\n");
+    }
+
+    private static void printPrev() {
+        int c = 1;
+        for(String x : prev)
+            System.out.printf("%d. %s\n",c,x);
+        System.out.println();
+    }
+
+    private static String getExpression() {
         System.out.print("Input expression: ");
-        String input = in.nextLine();
+        String input = in.nextLine().trim();
         return input;
     }
-    private static int getInput()
-    {
-        while(true)
-        {
+
+    private static int getInput() {
+        while (true) {
             try {
                 int option = in.nextInt();
                 in.nextLine();
-                if(option > 0 && option < 5)
+                if (option > 0 && option < 5)
                     return option;
                 else
                     throw new Exception();
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Invalid option");
             }
         }
